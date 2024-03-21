bf4f2b2b2c0ffe157ceea4de224e5ca12262aa3f
[tariqbaater] <[tariqbaater@gmail.com]>
2024-03-21 22:42:16 +0300

    The diff shows changes made to the CSS and JavaScript files in a web application

--- public/js/apiCalls.mjs
+++ public/js/apiCalls.mjs
@@ -1,38 +1,38 @@
 //*********** API *************
 // read history data from db api
 export async function historyData(id) {
-  const response = await fetch(`http://localhost:8080/history/${id}`);
-  const data = await response.json();
-  return data;
+    const response = await fetch(`http://localhost:8080/history/${id}`);
+    const data = await response.json();
+    return data;
 }
 // read delivery data from db api
 export const deliveryData = async (id) => {
-  const response = await fetch(`http://localhost:8080/dry_delivery/${id}`);
-  const data = await response.json();
-  return data;
+    const response = await fetch(`http://localhost:8080/dry_delivery/${id}`);
+    const data = await response.json();
+    return data;
 };
 // read search data from db api
 export const loadData = async (query) => {
-  const response = await fetch(`http://localhost:8080/search?q=${query}`);
-  const data = await response.json();
-  return data;
+    const response = await fetch(`http://localhost:8080/search?q=${query}`);
+    const data = await response.json();
+    return data;
 };
 // read writeoff data from db api
 export const loadWriteOff = async () => {
-  const response = await fetch(`http://localhost:8080/writeoff/`);
-  const data = await response.json();
-  return data;
+    const response = await fetch(`http://localhost:8080/writeoff/`);
+    const data = await response.json();
+    return data;
 };
 // read high value data from db api
 export const loadHighValue = async () => {
-  const response = await fetch(`http://localhost:8080/high_value/`);
-  const data = await response.json();
-  return data;
+    const response = await fetch(`http://localhost:8080/high_value/`);
+    const data = await response.json();
+    return data;
 };
 
 // read missing availiability data from db api
 export const loadMissingAvailiability = async () => {
-  const response = await fetch(`http://localhost:8080/missing_availability/`);
-  const data = await response.json();
-  return data;
+    const response = await fetch(`http://localhost:8080/missing_availability/`);
+    const data = await response.json();
+    return data;
 };
